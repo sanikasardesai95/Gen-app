71d8bde3cd2acab3c9ee80192d0ffedc5959a720
Kunal080101 <kunalpatel1928@gmail.com>
2024-08-01 00:33:53 +0000

    docker file changed

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def generate():
     return render_template('index.html', generated_text=generated_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
